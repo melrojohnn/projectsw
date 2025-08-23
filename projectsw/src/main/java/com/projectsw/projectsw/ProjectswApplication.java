a1fcaef7ce9cee230f581f639041d4a4f92b198b
Johnnes Melro <johnnes.melro@gmail.com>
2025-08-23 15:13:06 -0300

    Initial commit: Spring Boot project setup

---
+++ projectsw/src/main/java/com/projectsw/projectsw/ProjectswApplication.java
@@ -0,0 +1,13 @@
+package com.projectsw.projectsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectswApplication.class, args);
+	}
+
+}
---
+++ projectsw/src/test/java/com/projectsw/projectsw/ProjectswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsw.projectsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
