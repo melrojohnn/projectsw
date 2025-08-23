79aa7a914fe6879db9a181010dd786698bd9edfc
Johnnes Melro <johnnes.melro@gmail.com>
2025-08-23 18:08:06 -0300

    add MissionRepository

---
+++ projectsw/src/main/java/com/projectsw/projectsw/repository/MissionRepository.java
@@ -0,0 +1,8 @@
+package com.projectsw.projectsw.repository;
+
+import com.projectsw.projectsw.model.MissionModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionRepository extends JpaRepository<MissionModel, Long> {
+
+}
